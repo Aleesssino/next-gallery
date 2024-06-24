203dd1ce07276f74b6dbb4e60f17d378f0dc895a
Aleesssino <alesg3a1b4o1@gmail.com>
2024-06-24 20:46:38 +0200

    chore: Add Zod & Zod schema for type validation

---
+++ src/models/TImages.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const ImageSchema = z.object({
+  id: z.string(),
+  description: z.string().optional(),
+  user: z.object({ username: z.string() }),
+  urls: z.object({ raw: z.string() }),
+  width: z.number(),
+  height: z.number(),
+});
+
+export const Images = z.array(ImageSchema);
+export type TImages = z.infer<typeof Images>;
+export type TImage = z.infer<typeof ImageSchema>;
