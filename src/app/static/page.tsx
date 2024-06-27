9457376f7c7cf2112cdbab39c2bd6fbeaa36b6c2
Aleesssino <alesg3a1b4o1@gmail.com>
2024-06-27 16:17:58 +0200

    feat: Add metadata

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,5 +1,12 @@
+import InfoCard from "@/components/infoCard";
 import { TImage, ImageSchema } from "@/models/TImages";
+import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Static fetching - Next Gallery",
+};
 
 export default async function Page() {
   try {
@@ -16,29 +23,43 @@ export default async function Page() {
     // Validate the image data against the schema
     const validatedImage = ImageSchema.parse(unValidatedImage);
 
-    console.log(validatedImage);
-
     // Calculated width & height
-    const calculatedWidth = Math.min(400, validatedImage.width);
+    let size = 0;
+    if (validatedImage.width > validatedImage.height) {
+      size = 600;
+    } else {
+      size = 350;
+    }
+    const calculatedWidth = Math.min(size, validatedImage.width);
     const calculatedHeight =
       (calculatedWidth / validatedImage.width) * validatedImage.height;
 
     return (
-      <div className="flex flex-col pt-32 px-5 content-center justify-center items-center">
+      <div className="flex flex-col  px-5 content-center justify-center items-center pt-10">
+        <InfoCard> This page fetches and caches data at build time.</InfoCard>
+
         <Image
           src={validatedImage.urls.raw}
           alt={validatedImage.description || "Image"}
           width={calculatedWidth}
           height={calculatedHeight}
-          className={`w-[${calculatedWidth}] h-[${calculatedHeight}] rounded-2xl`}
+          className={`w-[${calculatedWidth}] h-[${calculatedHeight}] rounded-2xl shadow-white shadow-sm `}
         />
 
-        <p className="mt-2 text-lg font-bold">{validatedImage.description}</p>
-        <p className="mt-1">{validatedImage.user.username}</p>
+        <p className="mt-2 text-lg font-bold md:px-32">
+          {validatedImage.description}
+        </p>
+        <Link
+          href={`/users/${validatedImage.user.username}`}
+          className="mt-1 hover:border-stone-50 border-b-[1px]"
+        >
+          by {validatedImage.user.username}
+        </Link>
       </div>
     );
   } catch (error) {
     console.error("Error fetching or validating image data:", error);
+
     // You might want to handle errors more gracefully, e.g., showing an error message on the UI
     return (
       <div className="mt-7 flex justify-center items-center">
