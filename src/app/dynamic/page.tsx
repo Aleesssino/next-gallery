004feb4183e07a973a838c44c6f89d8057ab90c6
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-12 19:10:50 +0200

    feat: Add dynamic page

---
+++ src/app/dynamic/page.tsx
@@ -0,0 +1,19 @@
+import InfoCard from "@/components/infoCard";
+import UnsplashImage from "@/components/unsplashImage";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Dynamic fetching - Next Gallery",
+};
+
+export default async function Page() {
+  return (
+    <div className="flex flex-col  px-5 content-center justify-center items-center pt-10">
+      <InfoCard> This page fetches and caches data at build time.</InfoCard>
+      <Suspense fallback="loading...">
+        <UnsplashImage fetchType={"dynamic"} />
+      </Suspense>
+    </div>
+  );
+}
