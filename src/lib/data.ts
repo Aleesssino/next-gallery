2cfebc40575ab58969622e6c2ad0e7f09e09208b
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-12 20:25:10 +0200

    chore: Add isr fetch function

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -48,14 +48,42 @@ export async function fetchDynamicImageData() {
   }
 }
 
+// fetch data after 15 seconds
+export async function fetchISRimageData() {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}`,
+      // this will trigger cache after 15 seconds - same like add export const revalidate = 15 at the top of a page
+      { next: { revalidate: 15 } },
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch Images");
+    }
+
+    const unValidatedImage: TImage = await response.json();
+
+    // Validate the image data against the schema
+    const validatedImage = ImageSchema.parse(unValidatedImage);
+    return validatedImage;
+  } catch (error) {
+    console.error("Error fetching or validating image data:", error);
+
+    throw new Error("Failed to fetch data.");
+  }
+}
+
 // conditional fetch
-export type FetchType = "static" | "dynamic";
+export type FetchType = "static" | "dynamic" | "isr";
 export async function fetchData(fetchType: FetchType): Promise<TImage> {
   switch (fetchType) {
     case "static":
       return fetchStaticImageData();
     case "dynamic":
       return fetchDynamicImageData();
+    case "isr":
+      return fetchISRimageData();
+
     default:
       throw new Error(`Unsupported fetch type: ${fetchType}`);
   }
