41eafd42ee439d11566071ad6589c64d27f0f4c9
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-12 19:15:56 +0200

    chore: Implement calculateSize function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { TImage } from "@/models/TImages";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const calculateImageSize = (validatedImage: TImage) => {
+  let size = 0;
+  if (validatedImage.width > validatedImage.height) {
+    size = 600;
+  } else {
+    size = 350;
+  }
+
+  const calculatedWidth = Math.min(size, validatedImage.width);
+  const calculatedHeight =
+    (calculatedWidth / validatedImage.width) * validatedImage.height;
+
+  return { calculatedWidth, calculatedHeight };
+};
