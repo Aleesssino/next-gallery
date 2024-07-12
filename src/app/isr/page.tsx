bfd3af96ca739493921af08781f03060115034e5
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-12 20:25:30 +0200

    feat: Add ISR page

---
+++ src/app/isr/page.tsx
@@ -0,0 +1,19 @@
+import InfoCard from "@/components/infoCard";
+import UnsplashImage from "@/components/unsplashImage";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Incremantal Static Regeneration - Next Gallery",
+};
+
+export default async function Page() {
+  return (
+    <div className="flex flex-col  px-5 content-center justify-center items-center pt-10">
+      <InfoCard> This page uses incremantal static regenration.</InfoCard>
+      <Suspense fallback="loading...">
+        <UnsplashImage fetchType={"isr"} />
+      </Suspense>
+    </div>
+  );
+}
