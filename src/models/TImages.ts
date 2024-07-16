d1f127dedc0f5d702be3e94181df01ec218bf815
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-16 22:14:39 +0200

    chore: Update zod model

--- src/models/TImages.ts
+++ src/models/TImages.ts
@@ -4,6 +4,7 @@ export const ImageSchema = z.object({
   id: z.string(),
   description: z.string().nullable().optional(),
   color: z.string(),
+  blur_hash: z.string(),
   user: z.object({ username: z.string() }),
   urls: z.object({ raw: z.string() }),
   width: z.number(),
