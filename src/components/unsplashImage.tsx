f3c2172ab3ab8e067660550068fa2c8340855313
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-24 04:59:26 +0200

    chore: Upgrade Image component

--- src/components/unsplashImage.tsx
+++ src/components/unsplashImage.tsx
@@ -1,8 +1,5 @@
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { TImage } from "@/models/TImages";
-
 import { fetchData, FetchType } from "@/lib/data";
 import { calculateImageSize, getPlaceholderImageURL } from "@/lib/utils";
 
@@ -14,18 +11,19 @@ export default async function UnsplashImage({ fetchType }: fetchProps) {
   const imageData = await fetchData(fetchType);
   const { calculatedWidth, calculatedHeight, mobileSize } =
     calculateImageSize(imageData);
+
   return (
     <>
       <Image
-        src={imageData.urls.raw}
+        src={`${imageData.urls.raw}?w=${calculatedWidth}`}
         alt={imageData.description || "Image"}
+        className={`w-[${calculatedWidth}] h-[${calculatedHeight}] rounded-2xl shadow-white shadow-sm `}
+        sizes={`(max-width: 768px) ${mobileSize}px, (max-width: 1200px) ${calculatedWidth}px, ${calculatedWidth}px`}
         width={calculatedWidth}
         height={calculatedHeight}
-        sizes={`(max-width: 768px) ${mobileSize}px, (max-width: 1200px) ${calculatedWidth}px, ${calculatedWidth}px`}
-        placeholder="blur"
+        placeholder={"blur"}
         blurDataURL={getPlaceholderImageURL(imageData.urls.raw)}
-        className={`w-[${calculatedWidth}] h-[${calculatedHeight}] rounded-2xl shadow-white shadow-sm `}
-        loading="lazy"
+        priority={true}
       />
       <div className="text-center items-center md:px-52 pb-10">
         <p className="mt-2 text-lg font-bold md:px-32">
