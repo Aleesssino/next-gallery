a385d895c0a08dd72acb5b6bb36b57f9c5410757
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-24 04:58:08 +0200

    chore: Fix models

--- src/models/TImages.ts
+++ src/models/TImages.ts
@@ -3,14 +3,12 @@ import { z } from "zod";
 export const ImageSchema = z.object({
   id: z.string(),
   description: z.string().nullable().optional(),
-  color: z.string(),
-  blur_hash: z.string(),
   user: z.object({ username: z.string() }),
   urls: z.object({ raw: z.string() }),
   width: z.number(),
   height: z.number(),
 });
 
-export const Images = z.array(ImageSchema);
-export type TImages = z.infer<typeof Images>;
+export const GalleryImageSchema = z.array(ImageSchema);
+export type Tgallery = z.infer<typeof GalleryImageSchema>;
 export type TImage = z.infer<typeof ImageSchema>;
