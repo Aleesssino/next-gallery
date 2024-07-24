cd4c29acb7cc67517e7b7d0b4d23544274b97edc
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-24 04:57:09 +0200

    chore: Update Next.config - remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["images.unsplash.com", "plus.unsplash.com"],
+//   },
+// };
+
+// export default nextConfig;
+
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com" || "plus.unsplash.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
 };
-
 export default nextConfig;
