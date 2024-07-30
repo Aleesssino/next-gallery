a4b55665eb7136d6465b25329d3c9b3df49a2e15
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-30 22:18:15 +0200

    chore: Update About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,29 @@ export default function Home() {
     <main className="text-center pt-12 px-5">
       <h1 className="font-bold mb-5 text-4xl md:text-5xl">Next Gallery</h1>
       <InfoCard>
-        <section>
-          <p>
-            This project showcases Next.js 14&apos;s app router and its key
-            features:
-          </p>
+        <p>
+          This project showcases Next.js 14&apos;s app router and its key
+          features:
+        </p>
 
-          <ul>
-            <li>Static and dynamic server-side rendering</li>
-            <li>Incremental static regeneration</li>
-            <li>Route handlers (API endpoints)</li>
-            <li>Metadata API</li>
-            <li>And more</li>
-          </ul>
+        <ul>
+          <li>Static and dynamic server-side rendering</li>
+          <li>Incremental static regeneration</li>
+          <li>Route handlers (API endpoints)</li>
+          <li>Metadata API</li>
+          <li>And more</li>
+        </ul>
 
-          <p>The website utilizes the Unsplash API for image content.</p>
+        <p>The website utilizes the Unsplash API for image content.</p>
 
-          <p>Exploring Next.js 14 through a simple, elegant image showcase.</p>
-        </section>
+        <p>Exploring Next.js 14 through a simple, elegant image showcase.</p>
+
+        <p className="text-stone-400">
+          Unsplash has a free quota of 50 requests per hour so you might start
+          getting errors if you try too often.
+        </p>
       </InfoCard>
+      <div className="w-96 h-[800px] bg-stone-400"></div>
     </main>
   );
 }
