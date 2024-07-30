a44f0d6a8d779a37b720ee801926a6c74d7538a1
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-30 12:19:44 +0200

    chore: Add type ReactNode to InfoCard -> children can be string or HTML

--- src/components/infoCard.tsx
+++ src/components/infoCard.tsx
@@ -1,7 +1,7 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
 interface InfoCardProps {
-  children: string;
+  children: ReactNode;
 }
 
 const InfoCard: FC<InfoCardProps> = ({ children }) => {
